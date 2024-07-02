5b66ad34f116d0f8e326717fefa22483dd80a4cb
indiancomputer <joeybbillz@gmail.com>
2024-07-02 13:28:53 -0400

    Update About.tsx

--- src/FRIES-COIN-frontend/src/components/About.tsx
+++ src/FRIES-COIN-frontend/src/components/About.tsx
@@ -10,47 +10,46 @@ const About: React.FC<BuyProps> = ({ handleConnectWallet }) => {
     <div className="py-20 px-4 sm:px-6 lg:px-8">
       <h2 className="text-3xl font-bold text-[#fff] text-center font-rem">
         About{" "}
-        <span className="text-[#ffd543] font-rem font-extrabold">$FRYS</span>
+        <span className="text-[#ffd543] font-rem font-extrabold">$QUOKKA</span>
         Coin
       </h2>
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center">
         <div className="w-full md:w-1/2 pr-8">
           <div className="mt-10">
             <h3 className="text-2xl font-bold text-gray-500 font-rem">
-              Our Story
+              Are Story
             </h3>
             <p className="mt-4 text-lg text-gray-300 font-rem">
-              Fries Coin began with a simple yet compelling idea: to unite fries
-              enthusiasts worldwide through a unique cryptocurrency. As fries
+              Quokka Coin began with a simple yet compelling idea: to unite Quokkas
+              enthusiasts worldwide through a unique cryptocurrency. As Quokkas
               lovers ourselves, we recognized the need for a community where
-              people could share their love for fries, from recipes to favorite
-              fries spots.
+              people could share their love for Quokkas.
             </p>
             <h4 className="mt-6 text-xl font-semibold text-gray-500 font-rem">
-              Development
+              Devlopment
             </h4>
             <p className="mt-2 text-lg text-gray-300 font-rem">
-              Our team comprises seasoned developers, blockchain experts, and
-              fries aficionados. We leveraged the Internet Computer Protocol
+              Our team comprises seasoned developers, OGs in the ecosystem, blockchain experts, and
+              Quokkas aficionados. We leveraged the Internet Computer Protocol
               (ICP) to build a secure, scalable, and decentralized platform that
-              would serve as the backbone of Fries Coin. ICP's innovative
+              would serve as the backbone of Quokkas Coin. ICP's innovative
               technology ensures that our transactions are fast, reliable, and
               environmentally friendly.
             </p>
             <h4 className="mt-6 text-xl font-semibold text-gray-500 font-rem">
-              Vision
+              Vison
             </h4>
             <p className="mt-2 text-lg text-gray-300 font-rem">
-              We aim to become the go-to digital currency for all things fries.
-              Whether you're buying fries at your favorite restaurant or tipping
-              a great fries recipe online, Fries Coin will be your currency of
+              We aim to become the go-to digital currency for all things Quokkas.
+              Whether you're buying Quokkas at your favorite restaurant or tipping
+              a great Quokkas recipe online, Quokkas Coin will be your currency of
               choice. Join us in celebrating the crispy, golden goodness of
-              fries with Fries Coin.
+              Quokkas with Quokkas Coin.
             </p>
           </div>
         </div>
         <div className="w-full md:w-1/2 flex justify-center">
-          <img src={aboutImage} alt="Fries" className="w-80 h-auto" />
+          <img src={aboutImage} alt="Quokkas" className="w-80 h-auto" />
         </div>
       </div>
     </div>
