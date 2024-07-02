88426ac0a037b17f3e996cce6d236cd855dd9fcf
indiancomputer <joeybbillz@gmail.com>
2024-07-02 13:44:22 -0400

    Update FAQ.tsx

--- src/FRIES-COIN-frontend/src/components/FAQ.tsx
+++ src/FRIES-COIN-frontend/src/components/FAQ.tsx
@@ -2,11 +2,11 @@ import { PlaceholdersAndVanishInput } from "../components/ui/placeholders-and-va
 
 export default function FAQ() {
   const placeholders = [
-    "Why should I buy $FRIES?",
+    "Why should I buy $QUOKKA?",
     "What is a meme coin?",
-    "What is the utility of $FRIES Coin?",
-    "Does $FRIES have any airdrops mechanism?",
-    "Does $FRIES have any unique features?",
+    "Am I getting scammed right now?",
+    "Does $QUOKKA even have a meme?",
+    "Will the devs do something???",
   ];
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -20,7 +20,7 @@ export default function FAQ() {
     <div className="h-[40rem] flex flex-col justify-center  items-center px-4">
       <h2 className="mb-10 sm:mb-20 text-xl text-center sm:text-5xl text-gray-100">
         Ask anything about{" "}
-        <span className="text-[#ffd543] font-semibold">$FRYS</span>
+        <span className="text-[#ffd543] font-semibold">$QUOKKA</span>
       </h2>
       <PlaceholdersAndVanishInput
         placeholders={placeholders}
